b9f060919f6fae88e623b328d54824c085bdc854
Dzikra-HP <forevelyn.r@gmail.com>
2017-11-24 11:06:15 +0700

    Initial commit

---
+++ IPDetector/IPDetector/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace IPDetector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StartButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.SubnetBox = new System.Windows.Forms.TextBox();
+            this.IPBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(255, 42);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(75, 23);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // StopButton
+            // 
+            this.StopButton.Enabled = false;
+            this.StopButton.Location = new System.Drawing.Point(336, 42);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(75, 23);
+            this.StopButton.TabIndex = 1;
+            this.StopButton.Text = "Stop";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // SubnetBox
+            // 
+            this.SubnetBox.Location = new System.Drawing.Point(73, 45);
+            this.SubnetBox.Name = "SubnetBox";
+            this.SubnetBox.Size = new System.Drawing.Size(166, 20);
+            this.SubnetBox.TabIndex = 2;
+            // 
+            // IPBox
+            // 
+            this.IPBox.Enabled = false;
+            this.IPBox.Location = new System.Drawing.Point(73, 89);
+            this.IPBox.Multiline = true;
+            this.IPBox.Name = "IPBox";
+            this.IPBox.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.IPBox.Size = new System.Drawing.Size(338, 191);
+            this.IPBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 297);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Waiting for input";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(471, 322);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.IPBox);
+            this.Controls.Add(this.SubnetBox);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.StartButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button StopButton;
+        private System.Windows.Forms.TextBox SubnetBox;
+        private System.Windows.Forms.TextBox IPBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ IPDetector/IPDetector/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Threading;
+
+namespace IPDetector
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+            SubnetBox.MaxLength = 11;         }
+
+        Thread myTherad = null;
+
+        public void scan (string subnet)
+        {
+            Ping ping;
+            PingReply reply;
+            IPAddress address;
+            IPHostEntry host;
+
+            for (int i = 250; i < 255; i++)
+            {
+                try
+                {
+                    string subnetn = "." + i.ToString();
+                    ping = new Ping();
+                    reply = ping.Send(subnet + subnetn);
+
+                    if (reply.Status == IPStatus.Success)
+                    {
+                        try
+                        {
+                            address = IPAddress.Parse(subnet + subnetn);
+                            host = Dns.GetHostEntry(address);
+
+                            IPBox.AppendText(subnet + "\t" + subnetn + "\t" + host.HostName.ToString() + "\t" + "UP\n");
+                        }
+                        catch
+                        {
+                        }
+                    }
+                    if (i < 254)
+                        label1.Text = "Checking " + SubnetBox.Text + "." + i;
+                    else
+                    {
+                        StopButton.Enabled = true;
+                        StartButton.Enabled = false;
+                        SubnetBox.Enabled = false;
+                        SubnetBox.Text = "";
+                        label1.Text = "Finished";
+                    }
+                }
+                catch
+                {
+
+                }
+                                    
+            }
+        }
+        private bool check()
+        {
+            //check if subnet box empty
+            if (SubnetBox.Text == "")
+            {
+                MessageBox.Show("Please input a valid first 9 digits of an IP");
+                SubnetBox.Text = "";
+                SubnetBox.Focus();
+                return false;
+            }
+            else
+                return true;
+            return false;
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+
+            if (check())
+            {
+                myTherad = new Thread(() => scan(SubnetBox.Text));
+                myTherad.Start();
+
+                if (myTherad.IsAlive)
+                {
+                    StopButton.Enabled = true;
+                    StartButton.Enabled = false;
+                    SubnetBox.Enabled = false;
+                }
+            }
+                    }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            //  myTherad.Suspend();
+            myTherad.Abort();
+            StartButton.Enabled = true;
+            StopButton.Enabled = false;
+            SubnetBox.Enabled = true;
+            SubnetBox.Text = "";
+            label1.Text = "Stopped";
+        }
+    }
+}
